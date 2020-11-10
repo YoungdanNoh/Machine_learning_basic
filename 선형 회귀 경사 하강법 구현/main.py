53b2c0b23d9f69fa9b80debdf111d1abb0dcfbcb
nohyoung <shdbwjd705270@gmail.com>
2020-11-10 23:01:17 +0900

    손실함수와 가설함수가 개선되는 것을 시각적인 그래프로 그려보기

--- 선형 회귀 경사 하강법 구현/main.py
+++ 선형 회귀 경사 하강법 구현/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def prediction(theta_0, theta_1, x):
     """주어진 학습 데이터 벡터 x에 대해서 모든 예측 값을 벡터로 리턴하는 함수"""
@@ -13,16 +14,23 @@ def prediction_difference(theta_0, theta_1, x, y):
     
 def gradient_descent(theta_0, theta_1, x, y, iterations, alpha):
     """주어진 theta_0, theta_1 변수들을 경사 하강를 하면서 업데이트 해주는 함수"""
-    for _ in range(iterations):  # 정해진 번만큼 경사 하강을 한다
-        error = prediction_difference(theta_0, theta_1, x, y)  # 예측값들과 입력 변수들의 오차를 계산
-        # 코드를 쓰세요
-        '''if (theta_0 > theta_0 - alpha*error.mean()) and (theta_1 > theta_1 - alpha*error.mean()):
-            theta_0 = theta_0 - alpha*error.mean()
-            theta_1 = theta_1 - alpha*error.mean()'''
+    m = len(x) # 총 데이터 개수
+    cost_list = [] # 경사하강을 한 번 할 때마다 그 시점의 손실을 저장하는 함수
+    for i in range(iterations):  # 정해진 번만큼 경사 하강을 한다
+        error = prediction_difference(theta_0, theta_1, x, y)  # 예측값들과 실제 값들의 오차를 계산
+        cost = (error@error) / (2*m) # 모든 오차를 재곱하고 더한 후 2m으로 나눈다.
+        cost_list.append(cost)
+        
         theta_0 = theta_0 - alpha*error.mean()
         theta_1 = theta_1 - alpha*(error*x).mean()
         
-    return theta_0, theta_1
+        # 실제 가설함수가 개선되는 것을 시각적으로 그려보기
+        if i % 10 == 0: # 그래프를 200번 그리지 않고 10번에 한번씩만 그리기
+            plt.scatter(house_size, house_price)
+            plt.scatter(house_size, prediction(theta_0, theta_1, x), color='red')
+            plt.show()
+        
+    return theta_0, theta_1, cost_list
     
     
 # 입력 변수(집 크기) 초기화 (모든 집 평수 데이터를 1/10 크기로 줄임)
@@ -36,7 +44,10 @@ theta_0 = 2.5
 theta_1 = 0
 
 # 학습률 0.1로 200번 경사 하강
-theta_0, theta_1 = gradient_descent(theta_0, theta_1, house_size, house_price, 200, 0.1)
+theta_0, theta_1, cost_list = gradient_descent(theta_0, theta_1, house_size, house_price, 200, 0.1)
 
-theta_0, theta_1
-    
+print(theta_0, theta_1)
+
+# 손실 리스트를 그래프에 그려보기
+plt.plot(cost_list)
+plt.show() # 경사하강을 반복할수록 손실이 줄어드는 것을 볼 수 있다.
